770dd0416a66024687e023f0b589cc478471d59d
Devon Deonarine <hello@devondeonarine.ca>
2020-10-07 15:41:00 -0400

    start/stop animation WIP

--- ios/RNImageSequence/RNImageSequenceManager.m
+++ ios/RNImageSequence/RNImageSequenceManager.m
@@ -5,6 +5,7 @@
 
 #import "RNImageSequenceManager.h"
 #import "RNImageSequenceView.h"
+#import "RCTUIManager.h"
 
 @implementation RNImageSequenceManager {
 }
@@ -13,6 +14,30 @@ RCT_EXPORT_MODULE();
 RCT_EXPORT_VIEW_PROPERTY(images, NSArray);
 RCT_EXPORT_VIEW_PROPERTY(framesPerSecond, NSUInteger);
 RCT_EXPORT_VIEW_PROPERTY(loop, BOOL);
+RCT_EXPORT_VIEW_PROPERTY(loopFrom, NSUInteger);
+RCT_EXPORT_VIEW_PROPERTY(loopTo, NSUInteger);
+
+RCT_EXPORT_METHOD(stopAnimating:(nonnull NSNumber *)reactTag){
+    [self.bridge.uiManager addUIBlock:^(RCTUIManager *uiManager, NSDictionary<NSNumber *,UIView *> *viewRegistry) {
+        RNImageSequenceView *view = viewRegistry[reactTag];
+        if (!view || ![view isKindOfClass:[RNImageSequenceView class]]) {
+            RCTLogError(@"Cannot find RNImageSequenceView with tag #%@", reactTag);
+            return;
+        }
+        [view stopAnimating];
+    }];
+}
+
+RCT_EXPORT_METHOD(startAnimating:(nonnull NSNumber *)reactTag){
+    [self.bridge.uiManager addUIBlock:^(RCTUIManager *uiManager, NSDictionary<NSNumber *,UIView *> *viewRegistry) {
+        RNImageSequenceView *view = viewRegistry[reactTag];
+        if (!view || ![view isKindOfClass:[RNImageSequenceView class]]) {
+            RCTLogError(@"Cannot find RNImageSequenceView with tag #%@", reactTag);
+            return;
+        }
+        [view startAnimating];
+    }];
+}
 
 - (UIView *)view {
     return [RNImageSequenceView new];
